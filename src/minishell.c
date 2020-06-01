64c415de64d72a816e1d990dfa2cd91cfb701008
rhendrik <rhendrik@student.wethinkcode.co.za>
2020-06-01 20:11:08 +0200

    lil bits

--- src/minishell.c
+++ src/minishell.c
@@ -1,18 +1,33 @@
 #include "../includes/minishell.h"
+#include "../includes/get_next_line.h"
+
+void print_prompt(void)
+{
+	ft_putstr("\\n||m:\n");
+}
 
 void mini_loop(void)
 {
 	//declare line, args and status
+	int status;
+	char *line;
 
+	status = 1;
 	//do... while loop
+	do {
 	
 	//print prompt
-	
+	print_prompt();	
+
 	//read line
+	get_next_line(0, &line);
+	ft_putendl(line);
 	
 	//set args
 	
 	//set statu
+	status = 0;
+	} while (status);
 	
 	// after do... while... free args and line
 }
