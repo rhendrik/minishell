e08dc498c1d8dc64f217403a699d203d41e3421e
rhendrik <rhendrik@student.wethinkcode.co.za>
2020-06-01 19:07:50 +0200

    aaaaaaaaagh

---
+++ includes/minishell.h
@@ -0,0 +1,13 @@
+#ifndef _MINISHELL_H_
+#define _MINISHELL_H_
+
+# include "../libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
---
+++ src/minishell.c
@@ -0,0 +1,11 @@
+#include "../includes/minishell.h"
+
+int main(void)
+{
+	// load config files
+	
+	//run command loop
+	
+	// perform shutdown/cleanup
+	return (0);
+}
